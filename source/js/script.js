399160c8ebf8d43836f7a11076119005f42cc745
lutskovMax <82098283+lutskovMax@users.noreply.github.com>
2021-05-03 12:36:31 +0300

    Add files via upload

---
+++ source/js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel(
+      {
+        items: 1,
+
+          dots: true,
+          navText: ["<img src='../../img/prev.png' >", "<img src='../../img/next.png'>"],
+          nav: true,
+          autoplay: true,
+          smartSpeed: 900,
+          rewind: true,
+          autoplayHoverPause:true,
+            loop: true,
+          responsiveClass: true,
+
+
+
+      }
+
+  );
+
+
+//$('select').niceSelect();
+
+    $('.header__burger').click(function (){
+        $('.header__burger,.header__menu').toggleClass('active')
+        $('body').toggleClass('lock');
+    });
+
+
+
+
+
+
+});
